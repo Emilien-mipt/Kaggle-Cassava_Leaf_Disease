45c63db000bb6da16bf47a334d0e2faf177a8f13
Emin <emin.tagiev@phystech.edu>
2020-12-21 11:26:18 +0300

    Train with ResNext101_32x8d Training parameters: 11 epochs (for best score); Adam (lr = 1e-3); batch_size = 16 Scores: Local CV Best Acc = 0.8147; Public Acc = 0.806

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class CFG:
     debug = False
     apex = False
     epochs = 50
-    model_name = "tv_resnet101"
+    model_name = "resnext101_32x8d"
     batch_size = 32
     size = 256
     num_workers = 8
