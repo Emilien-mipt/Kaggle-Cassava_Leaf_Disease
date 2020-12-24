e42d1a36e73f1cdf0412a783e0b9c616975bfb2d
Emin Tagiev <emin.tagiev@phystech.edu>
2020-12-24 12:25:08 +0000

    Training parameters: 3 epochs (for best score); Adam (lr = 1e-3); batch_size = 32 Scores: Local CV Best Acc = 0.8526; Public Acc = 0.847

--- config.py
+++ config.py
@@ -20,7 +20,7 @@ class CFG:
     debug = False
     apex = False
     epochs = 50
-    model_name = "tf_efficientnet_b4_ns"
+    model_name = "efficientnet_b3a"
     batch_size = 32
     size = 256
     num_workers = 8
