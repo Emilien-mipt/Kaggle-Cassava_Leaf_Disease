ed0a1620a45582dc8bd0c23a558a743331822527
Emin Tagiev <emin.tagiev@phystech.edu>
2021-02-06 12:43:53 +0300

    Combine all previous augs + cuda11 Score: Local = 0.8958, Public = 0.891

--- augmentations.py
+++ augmentations.py
@@ -28,6 +28,17 @@ def get_transforms(*, data):
             [
                 # Resize(CFG.size, CFG.size, p=1.0),
                 RandomResizedCrop(CFG.size, CFG.size, p=1.0),
+                RandomBrightnessContrast(
+                    p=0.5, brightness_limit=(-0.1, 0.1), contrast_limit=(-0.1, 0.1), brightness_by_max=False
+                ),
+                ShiftScaleRotate(
+                    p=0.5,
+                    shift_limit=(-0.3, 0.3),
+                    scale_limit=(-0.1, 0.1),
+                    rotate_limit=(-180, 180),
+                    interpolation=4,
+                    border_mode=4,
+                ),
                 CoarseDropout(
                     p=0.5, max_holes=12, max_height=12, max_width=12, min_holes=8, min_height=8, min_width=8
                 ),
